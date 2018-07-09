92a5a41afa675456699f79397c6c701d4e72e2c5
Muskein Singh <muskein.singh@flipkart.com>
2018-07-09 23:01:22 +0530

    chnages

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import { LayoutProvider, GridLayoutProvider, BaseLayoutProvider, Dimension } from "./LayoutProvider";
-import { LayoutManager, GridLayoutManager, WrapGridLayoutManager, Point, Layout } from "./LayoutManager";
+import { GridLayoutProvider, Dimension } from "./LayoutProvider";
+import { GridLayoutManager, Point, Layout } from "./LayoutManager";
 
 export {
-    LayoutProvider,
     GridLayoutProvider,
-    BaseLayoutProvider,
-    LayoutManager,
     GridLayoutManager,
-    WrapGridLayoutManager,
     Dimension,
     Point,
     Layout,
