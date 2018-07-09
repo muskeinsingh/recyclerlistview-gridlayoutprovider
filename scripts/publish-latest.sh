653fa90dce57c28556b7d877fd899580159e595a
Muskein Singh <muskein.singh@flipkart.com>
2018-07-09 23:03:32 +0530

    removing mention of internal registry

--- scripts/publish-latest.sh
+++ scripts/publish-latest.sh
@@ -7,9 +7,6 @@ if [ "$CONT" = "y" ]; then
 npm run clean
 npm run build
 
-echo "Pushing out LATEST build to internal registry..."
-npm config set registry http://10.85.59.116/artifactory/v1.0/artifacts/npm/
-npm publish
 echo "Pushing out LATEST build to default registry..."
 npm config set registry https://registry.npmjs.org
 npm publish
