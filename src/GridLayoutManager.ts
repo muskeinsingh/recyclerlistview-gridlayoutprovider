4a7f20c428950dba22b843414f9846d0926ca386
Muskein Singh <muskein.singh@flipkart.com>
2018-07-16 18:24:29 +0530

    removing unused import

--- src/GridLayoutManager.ts
+++ src/GridLayoutManager.ts
@@ -1,11 +1,6 @@
-/***
- * Computes the positions and dimensions of items that will be rendered by the list. The output from this is utilized by viewability tracker to compute the
- * lists of visible/hidden item.
- */
 import {
   Dimension,
   Layout,
-  Point,
   WrapGridLayoutManager,
 } from "recyclerlistview";
 import GridLayoutProvider from "./GridLayoutProvider";
