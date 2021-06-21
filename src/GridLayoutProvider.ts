d5e0c67e7728af057481ab4682f109f38a72d521
Bhavya Dang <bhavya24dang@gmail.com>
2021-06-21 17:48:18 +0530

    fix: new layout added for few pages

--- src/GridLayoutProvider.ts
+++ src/GridLayoutProvider.ts
@@ -16,11 +16,13 @@ export class GridLayoutProvider extends LayoutProvider {
     // If horizonal return width while spans will be rowspans. Opposite holds true if not horizontal
     getHeightOrWidth: (index: number) => number,
     acceptableRelayoutDelta?: number,
+    // temp variable for showing buckets using precise layout
+    isPreciseLayout?: boolean
   ) {
     super(
       getLayoutType,
-      (type: string | number, dimension: Dimension, index: number) => {
-        this.setLayout(dimension, index);
+      (_: string | number, dimension: Dimension, index: number) => {
+        isPreciseLayout ? this.setPreciseLayout(dimension, index) : this.setLayout(dimension, index);
       },
     );
     this._getHeightOrWidth = getHeightOrWidth;
@@ -49,6 +51,26 @@ export class GridLayoutProvider extends LayoutProvider {
       } else {
         dimension.height = this._getHeightOrWidth(index);
         dimension.width = (this._renderWindowSize.width / maxSpan) * itemSpan;
+      }
+    } else {
+      throw new Error("setLayout called before layoutmanager was created, cannot be handled");
+    }
+  }
+
+  private setPreciseLayout(dimension: Dimension, index: number): void {
+    const maxSpan: number = this._maxSpan;
+    const itemSpan: number = this._getSpan(index);
+    if (itemSpan > maxSpan) {
+      throw new Error("Item span for index " + index + " is more than the max span");
+    }
+    if (this._renderWindowSize) {
+      if (this._isHorizontal) {
+        dimension.width = +(this._getHeightOrWidth(index).toFixed(2));
+        dimension.height = +(((this._renderWindowSize.height / maxSpan) * itemSpan).toFixed(2));
+
+      } else {
+        dimension.height = +(this._getHeightOrWidth(index).toFixed(2));
+        dimension.width = +(((this._renderWindowSize.width / maxSpan) * itemSpan).toFixed(2));
       }
     } else {
       throw new Error("setLayout called before layoutmanager was created, cannot be handled");
